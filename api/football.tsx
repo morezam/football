e393d54797fda3e3d7d733405dd4f2b758a59246
Mohammadreza <mohammadreza.ha.va@gmail.com>
2021-07-19 10:42:16 +0430

    add font

--- api/football.tsx
+++ api/football.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
 	baseURL: 'https://v3.football.api-sports.io',
 	headers: {
-		'x-rapidapi-key': process.env.RAPID_API_KEY,
+		'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
 		'x-rapidapi-host': 'v3.football.api-sports.io',
 	},
 });

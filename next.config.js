dd00192d805063eb5fb370d38dfad460b257b186
Mohammad Hamzehei <mohammadreza.ha.va@gmail.com>
2023-11-05 16:55:42 +0330

    change nextjs config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 module.exports = {
 	reactStrictMode: true,
 	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**.api-sports.io',
-			},
-		],
-		minimumCacheTTL: 31536000,
+		// remotePatterns: [
+		// 	{
+		// 		protocol: 'https',
+		// 		hostname: '**.api-sports.io',
+		// 	},
+		// ],
+		// minimumCacheTTL: 31536000,
+		unoptimized: true,
 	},
 };
